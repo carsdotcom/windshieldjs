bc9932a2afcc310ce070b42bcd37c513620fd322
JC Sokolow <jcsokolow@cars.com>
2016-02-11 10:51:49 -0600

    SR-0000 | Register all the templates on load.

--- test/windshield.js
+++ test/windshield.js
@@ -22,35 +22,7 @@ describe('windshield plugin', function () {
     });
 
     describe('registering', function () {
-        it('should log an error if the options are invalid', function (done) {
-            var opts = {};
-            registerWithOptions(opts, function(err) {
-                assert(err);
-                assert(/ValidationError/.test(err));
-                done();
-            });
-        });
 
-        it('should throw an error if the helpers directory can not be found', function (done) {
-            var baseDir = path.join(tmpBase, 'empty');
-            async.series([
-                mkdirp.bind(null, baseDir),
-                test
-            ], done);
-
-            function test(cb) {
-                var opts = {
-                    rootDir: baseDir,
-                    routes: []
-                };
-                registerWithOptions(opts, function(err) {
-                    assert(err);
-                    assert(/ENOENT/.test(err));
-                    assert(/helpers/.test(err));
-                    cb();
-                });
-            }
-        });
 
     });
 });
