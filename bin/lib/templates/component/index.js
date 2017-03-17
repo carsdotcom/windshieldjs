c97ed03a153a2ef91d217d84dd83661a8bf35141
Mac Heller-Ogden <mac@machellerogden.com>
2017-03-17 10:42:27 -0500

    removing windshield binary

--- bin/lib/templates/component/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const path = require('path');
-const windshield = require('windshieldjs');
-
-module.exports.adapter = require('./adapter');
-module.exports.templates = {
-    default: windshield.readTemplate(path.join(__dirname, 'templates/default.html'))
-};
